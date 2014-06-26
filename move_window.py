fe39bfe62987bf7b4c691462f45962c5fcd0188a
Eric Battenberg <ebattenberg@gracenote.com>
2014-06-26 16:11:21 -0700

    add config file / autoconfig support

--- move_window.py
+++ move_window.py
@@ -6,49 +6,117 @@ import sys
 import pdb
 import json
 import re
+import os
+
+# ------------------------------------------
+# config path
+# ------------------------------------------
+
+config_filepath = os.path.expanduser('~/.scripts/move_window_conf.json')
+
+# ------------------------------------------
+# helper functions 
+# ------------------------------------------
+
+def get_autoconfig(config_filepath):
+    config = {}
+    xrandr_info = sp.check_output(['xrandr'])
+    #xrandr_info = 'current 1920 x 1080,'
+    primary_match = re.search(
+            r' connected primary (\d+)x(\d+)\+(\d+)\+(\d+) ',
+            xrandr_info)
+    if primary_match:
+        p1,p2,p3,p4 = primary_match.group(1,2,3,4)
+        config['mon0_size'] = (int(p1),int(p2))
+        config['mon0_offset'] = (int(p3),int(p4))
+    else:
+        print 'unable to find primary monitor size'
+        config['mon0_size'] = (0,0)
+        config['mon0_offset'] = (0,0)
+
+    secondary_match = re.search(
+            r' connected (\d+)x(\d+)\+(\d+)\+(\d+) ',
+            xrandr_info)
+    if secondary_match:
+        s1,s2,s3,s4 = secondary_match.group(1,2,3,4)
+        config['mon1_size'] = (int(s1),int(s2))
+        config['mon1_offset'] = (int(s3),int(s4))
+    else:
+        print 'unable to find secondary monitor size'
+        config['mon1_size'] = (0,0)
+        config['mon1_offset'] = (0,0)
+
+    overall_size = re.search(r" current (\d+) x (\d+),",xrandr_info).group(1,2)
+    config['overall_size'] = tuple(int(x) for x in overall_size)
+
+    return config
+
+# ------------------------------------------
+# arguments
+# ------------------------------------------
+
+if len(sys.argv) < 2:
+    sys.exit('usage: python move_window.py {small_left,big_left,small_right,big_right,autoconfig}')
+
+arg = sys.argv[1]
+
+if arg == 'autoconfig':
+    # create conf from xrandr
+    config = get_autoconfig(config_filepath)
+    json.dump(config,open(config_filepath,'w'))
+    print 'created config file at %s' % config_filepath
+    sys.exit()
+
+# ------------------------------------------
+# config
+# ------------------------------------------
 
 try:
-    config = json.loads(open('move_window_conf.json').read())
+    config = json.load(open(config_filepath))
 except IOError:
     # create conf from xrandr
-        
+    print ''
+    print 'move_window.py: missing config file at %s' % config_filepath
+    print 'run "python move_window.py autoconfig" to create the config file'
+    print ''
+    sys.exit()
 
 
-# configuration ----------------------
+# ------------------------------------
+# params
+# ------------------------------------
 MON0_SIZE = config['mon0_size']
 MON1_SIZE = config['mon1_size']
 MON0_OFFSET = config['mon0_offset']
 MON1_OFFSET = config['mon1_offset']
-# ------------------------------------
+OVERALL_SIZE = config['overall_size']
 
-if len(sys.argv) < 2:
-    sys.exit('usage: python move_window.py {small_left,big_left,small_right,big_right}')
 
-arg = sys.argv[1]
+# -------------------------------------------
+# main
+# -------------------------------------------
 
-xrandr_info = sp.check_output(['xrandr'])
-#xrandr_info = 'current 1920 x 1080,'
-overall_size = re.search(r".*current (\d+) x (\d+),",xrandr_info).group(1,2)
-overall_size = [int(x) for x in overall_size]
-#print overall_size
+
+
 win = sp.check_output(['xdotool', 'getactivewindow']).strip()
 win_info = sp.check_output(['xwininfo','-id',win])
 
-if overall_size[0] == MON0_SIZE[0]+MON1_SIZE[0]:
+if OVERALL_SIZE[0] == MON0_SIZE[0]:
+    num_monitors = 1
+    monitor_size = [MON0_SIZE]
+    monitor_offset = [MON0_OFFSET]
+    active_monitor = 0
+elif OVERALL_SIZE[0] == MON0_SIZE[0]+MON1_SIZE[0]:
     num_monitors = 2
     monitor_size = [MON0_SIZE,MON1_SIZE]
     monitor_offset = [MON0_OFFSET,MON1_OFFSET]
+
 
     left_coord = int(re.search(r".*Absolute upper-left X: +(\d+)\n",win_info).group(1))
     if left_coord < monitor_offset[1][0]:
         active_monitor = 0
     else:
         active_monitor = 1
-elif overall_size[0] == MON0_SIZE[0]:
-    num_monitors = 1
-    monitor_size = [MON0_SIZE]
-    monitor_offset = [MON0_OFFSET]
-    active_monitor = 0
 else:
     raise ValueError, 'unsupported overall_size'
 
